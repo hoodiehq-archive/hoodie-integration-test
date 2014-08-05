9521046bcbdf1a1f095f788e61301ba8ee46b7a4
Stephan Bönnemann <stephan@boennemann.me>
2014-08-05 11:47:16 +0200

    refactor(smoke): summarize smoke tests
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

---
+++ tests/spec/smoke.spec.js
@@ -0,0 +1,31 @@
+/*global casper:false */
+
+'use strict';
+
+var hosts = require('../hosts.json');
+
+casper.test.comment('check hosts');
+
+Object.keys(hosts).forEach(function(hostname) {
+  var host = hosts[hostname];
+
+  casper.start(host, function(resp) {
+
+    this.test.info('Current location is ' + this.getCurrentUrl());
+
+    this.test.assert(resp.status === 200);
+    this.test.assert(this.getCurrentUrl() === host);
+  });
+
+  casper.thenOpen(host + '_api', function (resp) {
+    this.test.info('Current location is ' + this.getCurrentUrl());
+    this.test.assert(resp.status === 200);
+  });
+
+  casper.run(function() {
+    this.test.done();
+  });
+
+});
+
+

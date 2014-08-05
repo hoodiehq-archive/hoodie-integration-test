4b8bdec44d4daacb9e02b9f170653d0c99110b31
Stephan Bönnemann <stephan@boennemann.me>
2014-08-05 12:25:11 +0200

    test(signup): inital spec
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- tests/pre-test.js
+++ tests/pre-test.js
@@ -1,2 +1,11 @@
 casper.test.comment('pre');
 casper.test.done();
+
+casper.on('page.error', function(msg, trace) {
+  this.echo('Error: ' + msg, 'ERROR');
+  for(var i=0; i<trace.length; i++) {
+    var step = trace[i];
+    this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
+  }
+});
+

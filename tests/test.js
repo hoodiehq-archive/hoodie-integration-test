29ccf9c7d11a729993fbb262cc29623f9359dd9d
Stephan Bönnemann <stephan@boennemann.me>
2014-08-14 23:02:03 +0200

    test(www): add smoke and signup test
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,72 @@
 define([
   'intern!object',
-  'intern/chai!assert',
+  'intern/chai!expect',
   'require',
   '../hosts.js'
-], function (registerSuite, assert, require, hosts) {
+], function (registerSuite, expect, require, hosts) {
   registerSuite({
-    name: 'index',
+    name: 'www',
 
-    'greeting form': function () {
+    'smoke': function() {
       return this.remote
-        .get(require.toUrl(hosts.www))
-        .setFindTimeout(5000)
-        .findByCssSelector('body');
+        .get(hosts.www)
+        .getPageTitle()
+        .then(function(title) {
+          expect(title).to.equal('testtest');
+        });
+    },
+
+    'signup': function() {
+      return this.remote
+        .get(hosts.www)
+        .findByCssSelector('.hoodie-account-signedout button')
+          .click()
+        .end()
+        .setFindTimeout(1e4)
+        .findByName('username')
+          .type('hoodieuser')
+        .end()
+        .findByName('password')
+          .type('hoodiepassword')
+        .end()
+        .findByName('password_confirmation')
+          .type('hoodiepassword')
+        .end()
+        .findByCssSelector('form > div > div.modal-footer > button')
+          .getVisibleText()
+          .then(function(label) {
+            expect(label).to.equal('Sign Up');
+          })
+          .click()
+        .end()
+        .sleep(500)
+        .findByClassName('hoodie-account-signedin')
+          .getVisibleText()
+          .then(function(label) {
+            expect(label).to.match(/^Hello, /);
+          })
+        .end()
+        .findByClassName('hoodie-username')
+          .getVisibleText()
+          .then(function(label) {
+            expect(label).to.equal('hoodieuser');
+          })
+        .end()
+        .getCookies()
+        .then(function(cookies) {
+          expect(cookies[0].name).to.equal('AuthSession');
+        })
+        .execute(function() {
+          return {
+            id: hoodie.id(),
+            config: localStorage.getItem('_hoodie_config')
+          };
+        })
+        .then(function(data) {
+          var config = JSON.parse(data.config);
+          expect(data.id).to.equal(config._hoodieId);
+          expect(config['_account.username']).to.equal('hoodieuser');
+        });
     }
   });
 });

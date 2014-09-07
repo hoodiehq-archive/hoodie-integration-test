ae1fe0953f798d2fcd78e10dc9a8382b4d3e1cf0
Gregor Martynus <gregor@martynus.net>
2014-09-07 16:07:52 +0200

    test(api): hoodie.checkConnection() / isConnected()

---
+++ tests/www/api/connection.js
@@ -0,0 +1,78 @@
+/* global hoodie */
+
+module.exports = function(expect, hosts/*, user*/) {
+
+  return this.remote
+    .get(hosts.www)
+    .setExecuteAsyncTimeout(10000)
+
+    // preparations for events testing
+    .execute(function() {
+      window.events = [];
+      hoodie.on('disconnected', function() {
+        window.events.push('disconnected');
+      });
+      hoodie.on('reconnected', function() {
+        window.events.push('reconnected');
+      });
+    })
+
+    // hoodie.checkConnection to resolve
+    .executeAsync(function(callback) {
+      return hoodie.checkConnection().done(callback);
+    })
+
+    // hoodie.isConnected to return true
+    .execute(function() {
+      return hoodie.isConnected();
+    })
+    .then(function(isConnected) {
+      expect(isConnected).to.equal(true);
+    })
+
+    // when Hoodie Server cannot be reached,
+    // - hoodie.checkConnection to reject
+    // - 'disconnected' event to be triggered
+    .executeAsync(function(callback) {
+      hoodie.baseUrl = 'http://fail';
+      return hoodie.checkConnection().fail(function() {
+        callback(window.events);
+      });
+    })
+    .then(function(events) {
+      expect(events.length).to.equal(1);
+      expect(events[0]).to.equal('disconnected');
+    })
+
+    // hoodie.isConnected to return false
+    .execute(function() {
+      return hoodie.isConnected();
+    })
+    .then(function(isConnected) {
+      expect(isConnected).to.equal(false);
+    })
+
+    // when Hoodie Server can be reached again, resolve
+    // - hoodie.checkConnection to resolve
+    // - 'reconnected' event to be triggered
+    .executeAsync(function(callback) {
+      hoodie.baseUrl = '';
+      return hoodie.checkConnection().done(function() {
+        callback(window.events);
+      });
+    })
+    .then(function(events) {
+      expect(events.length).to.equal(2);
+      expect(events[1]).to.equal('reconnected');
+    })
+
+    // hoodie.isConnected to return true
+    .execute(function() {
+      return hoodie.isConnected();
+    })
+    .then(function(isConnected) {
+      expect(isConnected).to.equal(true);
+    })
+
+    ;
+};

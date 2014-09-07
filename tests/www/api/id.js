f7dd2d39b1ae5e127cf48563de5f61c98763fa64
Gregor Martynus <gregor@martynus.net>
2014-09-07 15:12:04 +0200

    test(api): hoodie.id() tests

---
+++ tests/www/api/id.js
@@ -0,0 +1,93 @@
+/* global hoodie */
+
+module.exports = function(expect, hosts, user) {
+
+  var currentHoodieId;
+  var accountHoodieId;
+  var username = user.name + '-id-' + Date.now();
+  var password = user.password;
+  return this.remote
+    // .get(hosts.www)
+    // .clearCookies()
+    // // not supported by Firefox it seams:
+    // // .clearLocalStorage()
+    // .execute(function() {
+    //   localStorage.clear();
+    // })
+
+    .get(hosts.www)
+    .setExecuteAsyncTimeout(10000)
+
+    // hoodie.id() to return random id
+    .execute(function() {
+      return hoodie.id();
+    })
+    .then(function(id) {
+      currentHoodieId = id;
+      expect(id).to.match(/[a-z0-9]{7}/);
+    })
+
+    // hoodie.id() persists page reload
+    .get(hosts.www)
+    .execute(function() {
+      return hoodie.id();
+    })
+    .then(function(id) {
+      expect(id).to.equal(currentHoodieId);
+    })
+
+    // hoodie.id() to change when account destroyed
+    .executeAsync(function(callback) {
+      hoodie.account.destroy().done(function() {
+        callback(hoodie.id());
+      });
+    })
+    .then(function(id) {
+      expect(id).not.to.equal(currentHoodieId);
+      currentHoodieId = id;
+    })
+
+    // hoodie.id() not to change when signing up
+    .executeAsync(function(username, password, callback) {
+      hoodie.account.signUp(username, password).done(function() {
+        callback(hoodie.id());
+      });
+    }, [username, password])
+    .then(function(id) {
+      expect(id).to.equal(currentHoodieId);
+      accountHoodieId = currentHoodieId;
+    })
+
+    // hoodie.id() to change when signing out
+    .executeAsync(function(callback) {
+      hoodie.account.signOut().done(function() {
+        callback(hoodie.id());
+      });
+    })
+    .then(function(id) {
+      expect(id).not.to.equal(currentHoodieId);
+      currentHoodieId = id;
+    })
+
+    // hoodie.id() to change when signing out
+    .executeAsync(function(callback) {
+      hoodie.account.signOut().done(function() {
+        callback(hoodie.id());
+      });
+    })
+    .then(function(id) {
+      expect(id).not.to.equal(currentHoodieId);
+      currentHoodieId = id;
+    })
+
+    // hoodie.id() to be set to account's hoodieId
+    .executeAsync(function(username, password, callback) {
+      hoodie.account.signIn(username, password).done(function() {
+        callback(hoodie.id());
+      });
+    }, [username, password])
+    .then(function(id) {
+      expect(id).to.equal(accountHoodieId);
+      currentHoodieId = accountHoodieId;
+    });
+};

f4d4d7938bf802f50a0dbb4c8d44d81c6d50c103
Gregor Martynus <gregor@martynus.net>
2014-09-07 15:39:43 +0200

    test(api): hoodie.request() tests

---
+++ tests/www/api/request.js
@@ -0,0 +1,77 @@
+/* global hoodie */
+
+module.exports = function(expect, hosts/*, user*/) {
+
+  return this.remote
+    .get(hosts.www)
+    .setExecuteAsyncTimeout(10000)
+
+    // hoodie.request('GET', '/').done to be called
+    .executeAsync(function(callback) {
+      return hoodie.request('GET', '/').done(callback);
+    })
+    .then(function(response) {
+      expect(typeof response).to.equal('object');
+      // FIXME: no clue why that fails:
+      //        expect(response).to.be.an(Object)
+    })
+
+    // hoodie.request('GET', '/').then to be called
+    .executeAsync(function(callback) {
+      return hoodie.request('GET', '/').then(function() {
+        return 1;
+      }).done(callback);
+    })
+    .then(function(value) {
+      expect(value).to.equal(1);
+    })
+
+    // hoodie.request('GET', '/nothinghere').fail to be called
+    .executeAsync(function(callback) {
+      return hoodie.request('GET', '/nothinghere').fail(callback);
+    })
+    .then(function(error) {
+
+      expect(error.toString()).to.match(/HoodieNotFoundError/);
+
+      // FIXME: no clue why that fails:
+      //        expect(error.name).to.equal('HoodieNotFoundError');
+    })
+
+    // hoodie.request('GET', '/nothinghere') to fail with HoodieNotFoundError
+    .executeAsync(function(callback) {
+      return hoodie.request('GET', '/nothinghere').fail(callback);
+    })
+    .then(function(error) {
+      expect(error.toString()).to.match(/HoodieNotFoundError/);
+
+      // FIXME: no clue why that fails:
+      //        expect(error.name).to.equal('HoodieNotFoundError');
+    })
+
+    // hoodie.request('GET', '/_config') to fail with HoodieUnauthorizedError
+    .executeAsync(function(callback) {
+      return hoodie.request('GET', '/_config').fail(callback);
+    })
+    .then(function(error) {
+      expect(error.toString()).to.match(/HoodieUnauthorizedError/);
+    })
+
+    // hoodie.request('GET', 'http://fail/_config') to fail with HoodieConnectionError
+    .executeAsync(function(callback) {
+      return hoodie.request('GET', 'http://fail/_config').fail(callback);
+    })
+    .then(function(error) {
+      expect(error.toString()).to.match(/HoodieConnectionError/);
+    });
+
+    // hoodie.request('GET', '<path to CORS enabled endpoint') to resolve with response
+    // FIXME: uncomment once Bearer Token is merged
+    // .executeAsync(function(callback) {
+    //   var corsUrl = 'http://localhost:' + location.port;
+    //   return hoodie.request('GET', corsUrl + '/_api').done(callback);
+    // })
+    // .then(function(response) {
+    //   expect(typeof response).to.equal('object');
+    // })
+};

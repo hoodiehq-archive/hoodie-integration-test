83c82bd48b215bd5e1232f1c241c6761e29b8d52
Gregor Martynus <gregor@martynus.net>
2014-09-09 17:34:21 +0200

    fix: run both www & admin tests

--- tests/test.js
+++ tests/test.js
@@ -6,8 +6,9 @@ define([
   'intern/dojo/node!../hosts.json',
   'intern/dojo/node!./www',
   'intern/dojo/node!./admin'
-], function (registerSuite, chai, chaiAsPromised, expect, hosts, www) {
+], function (registerSuite, chai, chaiAsPromised, expect, hosts, www, admin) {
   chai.use(chaiAsPromised);
   chai.should();
   registerSuite(www(expect, hosts));
+  registerSuite(admin(expect, hosts));
 });
